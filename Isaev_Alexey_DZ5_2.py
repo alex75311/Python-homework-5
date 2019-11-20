5ac717c28b83987c8cb831d1cc4b96e7fb06e4be
Isaev Aleksey <isaev.alex75311@gmail.com>
2019-11-21 00:02:15 +0300

    Lesson 2.

---
+++ Isaev_Alexey_DZ5_2.py
@@ -0,0 +1,7 @@
+with open('input_text.txt', 'r', encoding='utf-8') as f:
+    str_list = f.readlines()
+
+print(f'Кол-во строк в этом файле {len(str_list)}')
+for idx, el in enumerate(str_list):
+    print(f'Строка {idx + 1}: {el.splitlines()}')
+    print(f'Количество слов в {idx + 1} строке: {len(el.split())}')
